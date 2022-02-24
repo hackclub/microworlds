c73b85ce1c1c78752c4ea7eb41c3c88ac73c22b0
leomcelroy <leomcelroy@gmail.com>
2022-02-23 21:36:48 -0500

    Update defaultProg.js

--- js/defaultProg.js
+++ js/defaultProg.js
@@ -9,12 +9,10 @@ const t = createTurtle(w/2, h/2);
 
 t.setSize(3);
 
-for (let i = 0; i < 430; i += 1) {
-  t.setColor(\`hsl(\${0.5*i}, \${101}%, \${59}%)\`)
+for (let i = 0; i < 374; i += 1) {
+  t.setColor(\`hsl(\${2.5*i}, \${101}%, \${59}%)\`)
   t.forward(i);
-  t.right(87);
+  t.right(56);
 }
-
-
 
 `
